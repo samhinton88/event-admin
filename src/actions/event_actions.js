11f272100158cfb1a9f702f571668ba75a5c9211
sam hinton <sam.hinton88@gmail.com>
2018-07-07 10:46:16 +0100

    container dir stubs

--- src/actions/event_actions.js
+++ src/actions/event_actions.js
@@ -9,18 +9,26 @@ import {
 
 const BASE_URL = 'https://sleepy-retreat-30937.herokuapp.com/'
 
-export const createEvent = async (data) => {
+export const createEvent = async (data) => async dispatch => {
+  const newUser = await axios.post(`${BASE_URL}/api/events`, data);
 
+  dispatch({ type: EVENT_CREATE, payload: newUser });
 }
 
-export const editEvent = async (data) => {
+export const editEvent = async (data, eventId) => async dispatch => {
+  const editedEvent = await axios.put(`${BASE_URL}/api/events/${eventId}`, data)
 
+  dispatch({ type: EVENT_EDIT, payload: editedEvent })
 }
 
-export const deleteEvent = async (eventId) => {
+export const deleteEvent = async (eventId) => async dispatch => {
+  await axios.delete(`${BASE_URL}/api/events/${eventId}`);
 
+  dispatch({ type: EVENT_DELETE, payload: eventId })
 }
 
-export const fetchEvents = async () => {
+export const fetchEvents = async () => async dispatch => {
+  const events = await axios.get(`${BASE_URL}/api/events`);
 
+  dispatch({ type: FETCH_EVENTS, payload: events })
 }
--- src/reducers/event_reducer.js
+++ src/reducers/event_reducer.js
@@ -10,14 +10,39 @@ const defaultState = {};
 export default (state = defaultState, action) => {
   switch (action.type) {
     case EVENT_CREATE:
+      return {
+        ...state, [action.payload._id]: action.payload
+      }
 
     case EVENT_EDIT:
+      return {
+        ...state, [action.payload._id]: action.payload
+      }
 
     case EVENT_DELETE:
+      const newState = Object.assign({}, state);
+      delete newState[action.payload];
+      return  newState;
+
+      }
 
     case FETCH_EVENTS:
-
+      return {
+        ...state,
+        nomalise(action.payload)
+      }
   }
 
   return state;
+}
+
+
+function nomalise(data) {
+  // return object listing items by id
+
+  const out = {};
+
+  data.forEach((d) => out[d._id] = d);
+
+  return out;
 }
