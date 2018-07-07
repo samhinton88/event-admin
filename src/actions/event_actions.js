ce2a980a83ede707f4e7fe6bdb1285d6bc2cbfd8
sam hinton <sam.hinton88@gmail.com>
2018-07-07 09:15:00 +0100

    create event action creator boilerplate

---
+++ src/actions/event_actions.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import {
+  EVENT_CREATE,
+  EVENT_DELETE,
+  EVENT_EDIT,
+  FETCH_EVENTS
+} from './types';
+
+const BASE_URL = 'https://sleepy-retreat-30937.herokuapp.com/'
+
+export const createEvent = async (data) => {
+
+}
